4ce38bd0a93b164d9719e94461221684608e8314
IntriguingTiles <jnickw@outlook.com>
2025-05-26 23:24:01 -0600

    Add s390x platform

--- sdk_includes/public/build.h
+++ sdk_includes/public/build.h
@@ -91,6 +91,7 @@ Then you can use another oneliner to query all variables:
 #undef XASH_PSVITA
 #undef XASH_WASI
 #undef XASH_WASM
+#undef XASH_S390X
 
 //================================================================
 //
@@ -257,6 +258,9 @@ Then you can use another oneliner to query all variables:
 		#define XASH_64BIT 1
 	#endif
 	#define XASH_WASM 1
+#elif defined __s390x__
+	#define XASH_S390X 1
+	#define XASH_64BIT 1
 #else
 	#error "Place your architecture name here! If this is a mistake, try to fix conditions above and report a bug"
 #endif
--- sdk_includes/public/buildenums.h
+++ sdk_includes/public/buildenums.h
@@ -113,7 +113,8 @@ For more information, please refer to <http://unlicense.org/>
 #define ARCHITECTURE_E2K     7
 #define ARCHITECTURE_RISCV   8
 #define ARCHITECTURE_PPC     9
-#define ARCHITECTURE_WASM    10
+#define ARCHITECTURE_WASM  10
+#define ARCHITECTURE_S390X 11
 
 #if XASH_AMD64
 	#define XASH_ARCHITECTURE ARCHITECTURE_AMD64
@@ -133,6 +134,8 @@ For more information, please refer to <http://unlicense.org/>
 	#define XASH_ARCHITECTURE ARCHITECTURE_PPC
 #elif XASH_WASM
 	#define XASH_ARCHITECTURE ARCHITECTURE_WASM
+#elif XASH_S390X
+	#define XASH_ARCHITECTURE ARCHITECTURE_S390X
 #else
 	#error
 #endif
